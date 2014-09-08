a0de50bd60e0b7e4dd788952bb76bd3a432dda63
Koziolek <bjkuczynski@gmail.com>
2014-09-08 21:15:36 +0200

    backup

---
+++ beginning_fp/ch1/src/main/java/pl/koziolekweb/bfp/App.java
@@ -0,0 +1,13 @@
+package pl.koziolekweb.bfp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
